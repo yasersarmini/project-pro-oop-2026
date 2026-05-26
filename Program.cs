fa6effb807c1b0719d96c4b390041c0a4ba2bdbb
Yaser Sarmini <yaser.sarmini@fluxus.nu>
2026-05-26 10:35:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_pro_oop_2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
